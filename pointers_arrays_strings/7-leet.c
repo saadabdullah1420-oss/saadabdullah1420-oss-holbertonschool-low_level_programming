be8c5f21fc7a791f9f6709ed7c9d08fa88bba8a9
saadabdullah1420-oss <saadabdullah1420@gmail.com>
2026-07-18 19:53:37 +0300

    Add leet function

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * leet - encodes a string into 1337
+ * @str: string to encode
+ *
+ * Return: pointer to the string
+ */
+char *leet(char *str)
+{
+	char letters[] = "aAeEoOtTlL";
+	char numbers[] = "4433007711";
+	int i;
+	int j;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		j = 0;
+		while (letters[j] != '\0')
+		{
+			if (str[i] == letters[j])
+				str[i] = numbers[j];
+			j++;
+		}
+		i++;
+	}
+
+	return (str);
+}
