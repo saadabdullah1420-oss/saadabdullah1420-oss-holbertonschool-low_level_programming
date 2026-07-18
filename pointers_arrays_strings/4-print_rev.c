81d155749551113130c5825d83ccd3048efcda79
saadabdullah1420-oss <saadabdullah1420@gmail.com>
2026-07-18 19:24:16 +0300

    Add print_rev function

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse
+ * @s: pointer to the string
+ */
+void print_rev(char *s)
+{
+	int len;
+
+	len = 0;
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+
+	while (len > 0)
+	{
+		len--;
+		_putchar(s[len]);
+	}
+	_putchar('\n');
+}
