b599718d2091a214f0349d7f9cf150c3b0444632
saadabdullah1420-oss <saadabdullah1420@gmail.com>
2026-07-18 20:11:07 +0300

    Implement addition operation

--- simple-calculator/calculator.c
+++ simple-calculator/calculator.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - simple calculator skeleton
+ * main - simple calculator menu loop
  *
  * Return: Always 0
  */
@@ -9,18 +9,28 @@ int main(void)
 {
 	int choice;
 
-	printf("Simple Calculator\n");
-	printf("1) Add\n");
-	printf("2) Subtract\n");
-	printf("3) Multiply\n");
-	printf("4) Divide\n");
-	printf("0) Quit\n");
-	printf("Choice: ");
+	while (1)
+	{
+		printf("Simple Calculator\n");
+		printf("1) Add\n");
+		printf("2) Subtract\n");
+		printf("3) Multiply\n");
+		printf("4) Divide\n");
+		printf("0) Quit\n");
+		printf("Choice: ");
 
-	scanf("%d", &choice);
+		scanf("%d", &choice);
 
-	if (choice == 0)
-		printf("Bye!\n");
+		if (choice == 0)
+		{
+			printf("Bye!\n");
+			break;
+		}
+		else if (choice < 1 || choice > 4)
+		{
+			printf("Invalid choice\n");
+		}
+	}
 
 	return (0);
 }
