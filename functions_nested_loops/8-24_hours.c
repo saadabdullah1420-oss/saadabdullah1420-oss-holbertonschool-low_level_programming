0aa93b3d0fcd4e018b9d7534410bd6f64b5237fa
saadabdullah1420-oss <saadabdullah1420@gmail.com>
2026-07-18 20:20:58 +0300

    Add jack bauer function

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints every minute of the day
+ *
+ * Return: Nothing
+ */
+void jack_bauer(void)
+{
+	int hour;
+	int minute;
+
+	hour = 0;
+
+	while (hour < 24)
+	{
+		minute = 0;
+
+		while (minute < 60)
+		{
+			_putchar((hour / 10) + '0');
+			_putchar((hour % 10) + '0');
+			_putchar(':');
+			_putchar((minute / 10) + '0');
+			_putchar((minute % 10) + '0');
+			_putchar('\n');
+
+			minute++;
+		}
+
+		hour++;
+	}
+}
