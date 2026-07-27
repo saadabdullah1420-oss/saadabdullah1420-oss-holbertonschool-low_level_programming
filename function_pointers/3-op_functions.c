022137afb4e9a316f7d4f8b179d447ee042b8b57
saadabdullah1420-oss <saadabdullah1420@gmail.com>
2026-07-27 17:43:09 +0300

    Fix Betty documentation for calculator functions

--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -1,25 +1,60 @@
 #include "3-calc.h"
 
+/**
+ * op_add - adds two numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: sum of a and b
+ */
 int op_add(int a, int b)
 {
 	return (a + b);
 }
 
+/**
+ * op_sub - subtracts two numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: difference of a and b
+ */
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
 
+/**
+ * op_mul - multiplies two numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: product of a and b
+ */
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
 
+/**
+ * op_div - divides two numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: division of a by b
+ */
 int op_div(int a, int b)
 {
 	return (a / b);
 }
 
+/**
+ * op_mod - gets remainder of division
+ * @a: first number
+ * @b: second number
+ *
+ * Return: remainder of a divided by b
+ */
 int op_mod(int a, int b)
 {
 	return (a % b);
