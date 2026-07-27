9312e2d98eaad94b7b9cbe25410457d97cdb75f4
saadabdullah1420-oss <saadabdullah1420@gmail.com>
2026-07-27 18:05:42 +0300

    Fix print_all function

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -2,8 +2,58 @@
 #include <stdio.h>
 
 /**
+ * print_char - prints a char
+ * @args: arguments list
+ *
+ * Return: Nothing
+ */
+void print_char(va_list args)
+{
+	printf("%c", va_arg(args, int));
+}
+
+/**
+ * print_int - prints an integer
+ * @args: arguments list
+ *
+ * Return: Nothing
+ */
+void print_int(va_list args)
+{
+	printf("%d", va_arg(args, int));
+}
+
+/**
+ * print_float - prints a float
+ * @args: arguments list
+ *
+ * Return: Nothing
+ */
+void print_float(va_list args)
+{
+	printf("%f", va_arg(args, double));
+}
+
+/**
+ * print_string - prints a string
+ * @args: arguments list
+ *
+ * Return: Nothing
+ */
+void print_string(va_list args)
+{
+	char *str;
+
+	str = va_arg(args, char *);
+	if (str == NULL)
+		printf("(nil)");
+	if (str != NULL)
+		printf("%s", str);
+}
+
+/**
  * print_all - prints anything
- * @format: list of types of arguments
+ * @format: list of types
  *
  * Return: Nothing
  */
@@ -11,33 +61,31 @@ void print_all(const char * const format, ...)
 {
 	va_list args;
 	unsigned int i = 0;
-	char *str;
+	unsigned int j;
+	char *separator = "";
+	char types[] = {'c', 'i', 'f', 's'};
+	void (*functions[])(va_list) = {
+		print_char,
+		print_int,
+		print_float,
+		print_string
+	};
 
 	va_start(args, format);
 
 	while (format != NULL && format[i])
 	{
-		if (i != 0)
-			printf(", ");
+		j = 0;
 
-		switch (format[i])
+		while (j < 4)
 		{
-		case 'c':
-			printf("%c", va_arg(args, int));
-			break;
-		case 'i':
-			printf("%d", va_arg(args, int));
-			break;
-		case 'f':
-			printf("%f", va_arg(args, double));
-			break;
-		case 's':
-			str = va_arg(args, char *);
-			if (str == NULL)
-				printf("(nil)");
-			else
-				printf("%s", str);
-			break;
+			if (format[i] == types[j])
+			{
+				printf("%s", separator);
+				functions[j](args);
+				separator = ", ";
+			}
+			j++;
 		}
 
 		i++;
