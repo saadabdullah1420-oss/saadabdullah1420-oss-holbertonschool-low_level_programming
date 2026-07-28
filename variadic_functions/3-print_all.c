f6f829cafab58f98db03e8f6fc03915f9ef9950e
saadabdullah1420-oss <saadabdullah1420@gmail.com>
2026-07-28 18:15:01 +0300

    Add 3-print_all

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -3,42 +3,78 @@
 #include <stdio.h>
 
 /**
+ * print_char - prints char
+ * @args: argument list
+ */
+void print_char(va_list args)
+{
+	printf("%c", va_arg(args, int));
+}
+
+/**
+ * print_int - prints integer
+ * @args: argument list
+ */
+void print_int(va_list args)
+{
+	printf("%d", va_arg(args, int));
+}
+
+/**
+ * print_float - prints float
+ * @args: argument list
+ */
+void print_float(va_list args)
+{
+	printf("%f", va_arg(args, double));
+}
+
+/**
+ * print_string - prints string
+ * @args: argument list
+ */
+void print_string(va_list args)
+{
+	char *s = va_arg(args, char *);
+
+	if (!s)
+		s = "(nil)";
+	printf("%s", s);
+}
+
+/**
  * print_all - prints anything
- * @format: list of types of arguments
+ * @format: list of types
  */
 void print_all(const char * const format, ...)
 {
 	va_list args;
-	int i = 0;
-	char *sep = "";
-	char *str;
+	int i = 0, j;
+	char *separator = "";
+
+	void (*funcs[])(va_list) = {
+		print_char,
+		print_int,
+		print_float,
+		print_string
+	};
+
+	char types[] = {'c', 'i', 'f', 's'};
 
 	va_start(args, format);
 
 	while (format && format[i])
 	{
-		if (format[i] == 'c')
+		j = 0;
+		while (j < 4)
 		{
-			printf("%s%c", sep, va_arg(args, int));
-			sep = ", ";
-		}
-		if (format[i] == 'i')
-		{
-			printf("%s%d", sep, va_arg(args, int));
-			sep = ", ";
-		}
-		if (format[i] == 'f')
-		{
-			printf("%s%f", sep, va_arg(args, double));
-			sep = ", ";
-		}
-		if (format[i] == 's')
-		{
-			str = va_arg(args, char *);
-			if (!str)
-				str = "(nil)";
-			printf("%s%s", sep, str);
-			sep = ", ";
+			if (format[i] == types[j])
+			{
+				printf("%s", separator);
+				funcs[j](args);
+				separator = ", ";
+			}
+			j++;
 		}
 		i++;
 	}
