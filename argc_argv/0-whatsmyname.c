0238b797675f24c6486584107ac05e19f92fb7eb
saadabdullah1420-oss <saadabdullah1420@gmail.com>
2026-07-28 17:59:10 +0300

    Move argc_argv to project root

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
 
+/**
+ * main - prints the program name
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: Always 0
+ */
 int main(int argc, char *argv[])
 {
-    (void)argc;
+	(void)argc;
 
-    printf("%s\n", argv[0]);
+	printf("%s\n", argv[0]);
 
-    return (0);
+	return (0);
 }
