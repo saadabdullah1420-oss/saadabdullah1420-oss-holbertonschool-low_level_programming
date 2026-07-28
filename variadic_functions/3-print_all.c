be36702716a9c22b003d2cc0fb6b21dcb1860497
saadabdullah1420-oss <saadabdullah1420@gmail.com>
2026-07-28 18:11:34 +0300

    Add 3-print_all

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,48 +1,48 @@
 #include "variadic_functions.h"
+#include <stdarg.h>
 #include <stdio.h>
 
 /**
  * print_all - prints anything
  * @format: list of types of arguments
- *
- * Return: Nothing
  */
 void print_all(const char * const format, ...)
 {
 	va_list args;
-	unsigned int i = 0;
+	int i = 0;
+	char *sep = "";
 	char *str;
 
 	va_start(args, format);
 
-	while (format != NULL && format[i])
+	while (format && format[i])
 	{
-		if (i != 0)
-			printf(", ");
-
 		if (format[i] == 'c')
-			printf("%c", va_arg(args, int));
-
+		{
+			printf("%s%c", sep, va_arg(args, int));
+			sep = ", ";
+		}
 		if (format[i] == 'i')
-			printf("%d", va_arg(args, int));
-
+		{
+			printf("%s%d", sep, va_arg(args, int));
+			sep = ", ";
+		}
 		if (format[i] == 'f')
-			printf("%f", va_arg(args, double));
-
+		{
+			printf("%s%f", sep, va_arg(args, double));
+			sep = ", ";
+		}
 		if (format[i] == 's')
 		{
 			str = va_arg(args, char *);
-
-			if (str == NULL)
+			if (!str)
 				str = "(nil)";
-
-			printf("%s", str);
+			printf("%s%s", sep, str);
+			sep = ", ";
 		}
-
 		i++;
 	}
 
-	printf("\n");
-
 	va_end(args);
+	printf("\n");
 }
