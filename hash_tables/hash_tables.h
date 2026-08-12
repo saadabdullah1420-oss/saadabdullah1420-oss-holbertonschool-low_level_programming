f26afceb43f64aaa166d8b2a10c4d9a92ad2818b
saadabdullah1420-oss <saadabdullah1420@gmail.com>
2026-08-12 19:36:33 +0300

    Add key index function

--- hash_tables/hash_tables.h
+++ hash_tables/hash_tables.h
@@ -4,6 +4,7 @@
 #include <stddef.h>
 
 unsigned long int hash_djb2(const unsigned char *str);
+unsigned long int key_index(const unsigned char *key, unsigned long int size);
 
 /**
  * struct hash_node_s - Node of a hash table
