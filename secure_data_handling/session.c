ab2438f64c788fbaa15534cb66a33e33a81165b0
saadabdullah1420-oss <saadabdullah1420@gmail.com>
2026-08-12 20:12:15 +0300

    Fix secure data handling memory management

---
+++ secure_data_handling/session.c
@@ -0,0 +1,89 @@
+#include <stdlib.h>
+#include <string.h>
+#include "session.h"
+
+session_t *session_create(const char *id, unsigned int uid,
+			  const unsigned char *data, size_t data_len)
+{
+	session_t *s;
+
+	if (!id)
+		return NULL;
+
+	s = malloc(sizeof(*s));
+	if (!s)
+		return NULL;
+
+	s->id = strdup(id);
+	if (!s->id)
+	{
+		free(s);
+		return NULL;
+	}
+
+	s->uid = uid;
+	s->data = NULL;
+	s->data_len = 0;
+
+	if (data_len > 0)
+	{
+		if (!data)
+		{
+			free(s->id);
+			free(s);
+			return NULL;
+		}
+
+		s->data = malloc(data_len);
+		if (!s->data)
+		{
+			free(s->id);
+			free(s);
+			return NULL;
+		}
+
+		memcpy(s->data, data, data_len);
+		s->data_len = data_len;
+	}
+
+	return s;
+}
+
+int session_set_data(session_t *s, const unsigned char *data, size_t data_len)
+{
+	unsigned char *tmp;
+
+	if (!s)
+		return 0;
+
+	if (data_len == 0)
+	{
+		free(s->data);
+		s->data = NULL;
+		s->data_len = 0;
+		return 1;
+	}
+
+	if (!data)
+		return 0;
+
+	tmp = realloc(s->data, data_len);
+	if (!tmp)
+		return 0;
+
+	memcpy(tmp, data, data_len);
+	s->data = tmp;
+	s->data_len = data_len;
+
+	return 1;
+}
+
+void session_destroy(session_t *s)
+{
+	if (!s)
+		return;
+
+	free(s->id);
+	free(s->data);
+	free(s);
+}
