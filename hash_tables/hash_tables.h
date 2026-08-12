6f9eb5b96323333cdeaa64404627a267f788c1d4
saadabdullah1420-oss <saadabdullah1420@gmail.com>
2026-08-12 19:37:25 +0300

    Add hash table set function

--- hash_tables/hash_tables.h
+++ hash_tables/hash_tables.h
@@ -5,6 +5,7 @@
 
 unsigned long int hash_djb2(const unsigned char *str);
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
+int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 
 /**
  * struct hash_node_s - Node of a hash table
