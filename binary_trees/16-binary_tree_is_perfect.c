8e5574fb6b1dcdbc7e77d378c4794b57e8b9bbd9
saadabdullah1420-oss <saadabdullah1420@gmail.com>
2026-08-16 18:12:03 +0300

    Add binary tree perfect check

--- binary_trees/16-binary_tree_is_perfect.c
+++ binary_trees/16-binary_tree_is_perfect.c
@@ -13,16 +13,16 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
-
-	if (left_height != right_height)
-		return (0);
-
 	if (tree->left == NULL && tree->right == NULL)
 		return (1);
 
 	if (tree->left == NULL || tree->right == NULL)
+		return (0);
+
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
+
+	if (left_height != right_height)
 		return (0);
 
 	return (binary_tree_is_perfect(tree->left) &&
