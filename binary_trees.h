1f0ce32ae297c01e5518915e0103bac5bd86a2f3
saadabdullah1420-oss <saadabdullah1420@gmail.com>
2026-08-16 18:05:59 +0300

    Add binary tree nodes function

--- binary_trees.h
+++ binary_trees.h
@@ -41,3 +41,5 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
 size_t binary_tree_size(const binary_tree_t *tree);
 
 size_t binary_tree_leaves(const binary_tree_t *tree);
+
+size_t binary_tree_nodes(const binary_tree_t *tree);
