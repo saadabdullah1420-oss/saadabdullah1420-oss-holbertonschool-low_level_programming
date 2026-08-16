29f2e6ce22df0958899ecfcfed44febaca2332fe
saadabdullah1420-oss <saadabdullah1420@gmail.com>
2026-08-16 18:01:53 +0300

    Add binary tree size function

--- binary_trees/11-binary_tree_size.c
+++ binary_trees/11-binary_tree_size.c
@@ -8,14 +8,9 @@
  */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-	size_t size;
-
 	if (tree == NULL)
 		return (0);
 
-	size = 1;
-	size += binary_tree_size(tree->left);
-	size += binary_tree_size(tree->right);
-
-	return (size);
+	return (1 + binary_tree_size(tree->left)
+		+ binary_tree_size(tree->right));
 }
