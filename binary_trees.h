c294673ff5718a4b3bba1545ebe48ca2f49b1932
saadabdullah1420-oss <saadabdullah1420@gmail.com>
2026-08-16 18:05:10 +0300

    Add binary tree leaves function

--- binary_trees.h
+++ binary_trees.h
@@ -39,3 +39,5 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
 
 size_t binary_tree_size(const binary_tree_t *tree);
+
+size_t binary_tree_leaves(const binary_tree_t *tree);
